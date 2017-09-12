82f2c383f43e2fb0b44b26b415cf1324ea7bbf03
Windi Wulan Dari <windiwulandari84@gmail.com>
2017-09-12 14:39:56 +0700

    Delete hapusanggota.php

--- hapusanggota.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once "config/koneksi.php";
-	$idhapus=$_GET['idhapus'];
-	$sql_hapus="DELETE FROM yo_anggota  WHERE id_anggota='$idhapus'";
-	mysql_query($sql_hapus, $konek)or die ("Gagal perintah hapus".mysql_error());
-?>
-<script language="JavaScript">
-document.location=('./')</script>a
