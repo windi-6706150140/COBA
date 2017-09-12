f74309e50fddb765c77f21492e64ce0948aaea18
Windi Wulan Dari <windiwulandari84@gmail.com>
2017-09-12 14:40:07 +0700

    Delete hapusbuku.php

--- hapusbuku.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once "config/koneksi.php";
-	$idhapus=$_GET['idhapus'];
-	$sql_hapus="DELETE FROM yo_buku  WHERE id_buku='$idhapus'";
-	mysql_query($sql_hapus, $konek)or die ("Gagal perintah hapus".mysql_error());
-?>
-<script language="JavaScript">
-document.location=('./')</script>a
